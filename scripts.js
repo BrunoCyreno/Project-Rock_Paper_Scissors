35d8083fccbc690b875d1be9df3e1c09ac6eb927
BrunoCyreno <114535946+BrunoCyreno@users.noreply.github.com>
2023-01-02 12:03:09 -0300

    add "computer choice" function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+function getComputerChoice(){
+    let computerSelection = Math.floor(Math.random() * 3);
 
+    if (computerSelection == 0) {
+        computerSelection = "Rock";
+    }
+    else if (computerSelection == 1) {
+        computerSelection = "Paper";
+    }
+    else {
+        computerSelection = "Scissors";
+    } 
+
+    return computerSelection
+}
+
+let result = getComputerChoice();
+console.log(result)
